875cf9938e14249358bb248179203826811bf45b
Lucas César <43433070+lucascda@users.noreply.github.com>
2024-05-23 15:30:47 -0300

    test: add ChangeFieldChar unit test

--- pkg/board/field_test.go
+++ pkg/board/field_test.go
@@ -37,3 +37,28 @@ func TestNewBoardField(t *testing.T) {
 		}
 	})
 }
+
+func TestChangeFieldChar(t *testing.T) {
+	t.Run("should change fieldChar to correct value", func(t *testing.T) {
+		tests := []struct {
+			name     string
+			value    string
+			expected string
+		}{
+			{"change value to X", "X", "X"},
+			{"change value to W", "W", "W"},
+		}
+
+		for _, test := range tests {
+			t.Run(test.name, func(t *testing.T) {
+				sut := NewBoardField()
+
+				sut.ChangeFieldChar(test.value)
+
+				if sut.fieldChar != test.expected {
+					t.Errorf("For=%v, expected=%s, got=%s", sut, test.expected, sut.fieldChar)
+				}
+			})
+		}
+	})
+}
